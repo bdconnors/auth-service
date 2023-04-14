44cd750f3bc464003500169c53edde63085090ec
bdconnors <bdc5435@rit.edu>
2023-04-14 14:14:11 -0400

    business layer tests

---
+++ Auth.Business.Tests/Dto/CreateOrgDto.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auth.Business.Tests.Dto
+{
+    public class CreateOrgDtoTests
+    {
+        [Fact]
+        public void CreateOrgDto_Success()
+        {
+            CreateOrgDto org = new CreateOrgDto() { Name = "Test Org" };
+            Assert.Equal("Test Org", org.Name);
+        }
+    }
+}
---
+++ Auth.Business.Tests/Dto/CreateSiteDtoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auth.Business.Tests.Dto
+{
+    public class CreateSiteDtoTests
+    {
+        [Fact]
+        public void CreateOrgDto_Success()
+        {
+            CreateSiteDto site = new CreateSiteDto() { OrgId = 0 , Name = "Test Site" };
+            Assert.Equal("Test Site", site.Name);
+            Assert.Equal(0, site.OrgId);    
+        }
+    }
+}
