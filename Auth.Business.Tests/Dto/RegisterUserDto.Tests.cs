a893a3f607aa7f54f0e60193bf30216e543669b3
bdconnors <bdc5435@rit.edu>
2023-05-05 09:02:16 -0400

    add DTO tests

---
+++ Auth.Business.Tests/Dto/RegisterUserDto.Tests.cs
@@ -0,0 +1,30 @@
+﻿using Auth.Business.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auth.Business.Tests.Dto
+{
+    public class RegisterUserDtoTests
+    {
+        [Fact]
+        public void RegisterUserDto_Success()
+        {
+            RegisterUserDto dto = new RegisterUserDto() { 
+                Email = "test@test.com", 
+                Password = "supersecure123@#",
+                FirstName = "test",
+                LastName = "tester",
+                MobileNumber = "5555555555"
+            };
+            Assert.Equal("test@test.com", dto.Email);
+            Assert.Equal("supersecure123@#", dto.Password);
+            Assert.Equal("test", dto.FirstName);
+            Assert.Equal("tester", dto.LastName);
+            Assert.Equal("5555555555", dto.MobileNumber);
+        }
+    }
+}
---
+++ Auth.Business.Tests/Dto/RegisterUserSiteDto.Tests.cs
@@ -0,0 +1,27 @@
+﻿using Auth.Business.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auth.Business.Tests.Dto
+{
+    public class RegisterUserDtoSiteTests
+    {
+        [Fact]
+        public void RegisterUserSiteDto_Success()
+        {
+            RegisterUserSiteDto dto = new RegisterUserSiteDto()
+            {
+                UserId = 0,
+                SiteId = 0,
+                Role = "ADMIN"
+            };
+            Assert.Equal(0, dto.UserId);
+            Assert.Equal(0, dto.SiteId);
+            Assert.Equal("ADMIN", dto.Role);
+        }
+    }
+}
